cf8686e5e50311b1171cabef435f1053ea40820d
MarwenKing15 <marweng.touati@gmail.com>
2021-11-16 16:56:57 +0100

    Fisrt commit

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const Profile = (props) => {
 const styleH1 = {color:"red" ,textAlign:"center"} ;
-const styleH2 = {textAlign:"center", fontFamily:"Verdana, Geneva, Tahoma, sans-serif;"}
+const styleH2 = {textAlign:"center", fontFamily:"Verdana, Geneva, Tahoma, sans-serif"}
 const centerDiv={height: "100%", width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}
 
 function handleName(ob){
@@ -17,22 +17,26 @@ function handleName(ob){
             <div style={centerDiv}>
                 <img  style={{borderRadius: "50%"}} src={props.children} alt="Profile Pic"/>
             </div>
+            <br/>
             <div>
             {/* Name */}
             <div>
                 <h1 style={styleH1}>Name:</h1>
+            <br/>
                 <h2 style={styleH2}>{props.fullName}</h2>
             </div>
-
+            <br/>
             {/* Bio */}
             <div>
                 <h1 style={styleH1}>Bio:</h1>
+            <br/>
                 <h2 style={styleH2}>{props.bio}</h2>
             </div>
-
+            <br/>
             {/* Profesion */}
             <div>
                 <h1 style={styleH1}>Profession:</h1>
+            <br/>
                 <h2 style={styleH2}>{props.prof}</h2>
             </div>
             </div>
