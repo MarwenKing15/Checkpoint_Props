e20b3402e58b6342ffc038699ba78b0a9feaae06
MarwenKing15 <marweng.touati@gmail.com>
2021-11-16 16:43:19 +0100

    first commit

---
+++ src/profile/Profile.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import PropTypes from "prop-types"; 
+
+const Profile = (props) => {
+const styleH1 = {color:"red" ,textAlign:"center"} ;
+const styleH2 = {textAlign:"center", fontFamily:"Verdana, Geneva, Tahoma, sans-serif;"}
+const centerDiv={height: "100%", width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}
+
+function handleName(ob){
+    alert("Welcome "+ob);
+}
+
+    return (
+        handleName(props.fullName),
+        <div>
+            {/* Image */}
+            <div style={centerDiv}>
+                <img  style={{borderRadius: "50%"}} src={props.children} alt="Profile Pic"/>
+            </div>
+            <div>
+            {/* Name */}
+            <div>
+                <h1 style={styleH1}>Name:</h1>
+                <h2 style={styleH2}>{props.fullName}</h2>
+            </div>
+
+            {/* Bio */}
+            <div>
+                <h1 style={styleH1}>Bio:</h1>
+                <h2 style={styleH2}>{props.bio}</h2>
+            </div>
+
+            {/* Profesion */}
+            <div>
+                <h1 style={styleH1}>Profession:</h1>
+                <h2 style={styleH2}>{props.prof}</h2>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+Profile.defaultProps={
+    fullName:"Marwen Touati",
+    bio:"21 years old male who loves F1, Video Games and Anime/Manga",
+    prof:"Computer Science Student/Web Developper",
+}
+
+Profile.propTypes={
+    fullName:PropTypes.string,
+    bio:PropTypes.string,
+    prof:PropTypes.string
+
+}
+
+export default Profile
